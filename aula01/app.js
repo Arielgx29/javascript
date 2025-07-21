aaa27161ce782b5bd173ce2e82a186fb726d3020
Arielgx29 <arielhenrique352@gmail.com>
2025-07-21 11:53:22 -0300

    curso de javascript

---
+++ aula01/app.js
@@ -0,0 +1,13 @@
+let age = 31        //let se usa para declarar uma variavel.
+let currentYear = 2019
+
+console.log(age, currentYear);
+
+age = 32
+console.log(age)     //pode dar outros valores para a mesma variavel, reatribuição.
+
+const points = 100 
+console.log(points)
+
+var score = 75
+console.log(score)
